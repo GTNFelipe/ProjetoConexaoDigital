9f91480663fa708d6c6fa1eb1e7cacf96a816ed1
Felipe Santana <felipestartt@gmail.com>
2025-05-22 15:19:51 -0300

    14:21 21/05

--- Aula1Java/src/Desafio8.java
+++ Aula1Java/src/Desafio8.java
@@ -4,32 +4,28 @@ public class Desafio8 {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
 
-        String menu;
+        String menu, nome = "Felipe Santana", conta = "Corrente";
         int opcao = 0;
-        double saldoEmConta = 5000.00;
-        double valorRecebido = 0;
+        double saldoEmConta = 5000.00, valorRecebido = 0;
 
+        System.out.println("-----------------------------");
+        System.out.println("\nDados iniciais do cliente:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo de conta: " + conta);
+        System.out.println("Saldo inicial: " + saldoEmConta);
+        System.out.println("\n-----------------------------");
 
         while (opcao != 4) {
-            menu = """
-                -----------------------------
-                  Dados iniciais do cliente:
-                
-                  Nome: Felipe Santana
-                  Tipo conta: Corrente
-                  Saldo inicial: R$ 5000,00
-                -----------------------------
-                
-                
-                Operações
-                
-                1- Consultar saldos
-                2- Receber valor
-                3- Transferir valor
-                4- Sair
-                
-                Digite a opção desejada:
-                """;
+            menu =   """
+                    \nOperações
+                    
+                    1- Consultar saldos
+                    2- Receber valor
+                    3- Transferir valor
+                    4- Sair
+                    
+                    Digite a opção desejada:
+                    """;
             System.out.println(menu);
             System.out.println("Digite uma opção:");
             opcao = leitura.nextInt();
@@ -37,26 +33,24 @@ public class Desafio8 {
             if (opcao == 1) {
                 System.out.println("Seu saldo atual é: R$" + saldoEmConta);
 
-            } else if (opcao == 2) {
+        } else if (opcao == 2) {
                 System.out.println("Qual valor será recebido?");
                 double valorAReceber = leitura.nextDouble();
-                valorRecebido = saldoEmConta+ valorAReceber;
-                System.out.println("Seu saldo atualizado é: R$" + valorRecebido);
+                saldoEmConta += valorAReceber; // Atualiza o saldo original
+                System.out.println("Seu saldo atualizado é: R$" + saldoEmConta);
 
             } else if (opcao == 3) {
-                System.out.println("Qual valor será transferido:");
-                double valorSaida = leitura.nextDouble();
+            System.out.println("Qual valor será transferido:");
+            double valorSaida = leitura.nextDouble();
 
+            if (valorSaida > saldoEmConta) {
+                System.out.println("Seu saldo em conta é menor que R$" + valorSaida + ", então você não poderá realizar a transferência ");
+            } else {
+                saldoEmConta -= valorSaida;
+                System.out.println("Seu saldo atualizado é: R$" + saldoEmConta);
+            }
 
-                if (valorSaida > valorRecebido) {
-                    System.out.println("Seu saldo em conta é menor que R$" + valorSaida + ", então você não poderá realizar a transferência ");
-
-                } else {
-                    double saldoAtual = saldoEmConta - valorSaida;
-                    System.out.println("Seu saldo atualizado é: R$" + saldoAtual);
-                }
-
-            } else if (opcao == 4) {
+        } else if (opcao == 4) {
                 System.out.println("Obrigado por utilizar nosso sistema, até mais!");
 
             } else
